01b8bee2a2fb8e3f22eef763f8923a5db3979ff4
Alessandro Gomes <112139213+allessandrogomes@users.noreply.github.com>
2023-04-15 23:33:17 -0300

    Atualização 15/04/23

---
+++ js/RemoverFiltroPeloX.js
@@ -0,0 +1,17 @@
+function removerFiltroPeloX() {
+    const btnsRemoverFiltro = document.querySelectorAll('.btnRemoverFiltro')
+    btnsRemoverFiltro.forEach((btnRemoverFiltro) => {
+        btnRemoverFiltro.addEventListener('click', (e) => {
+            const divPai = btnRemoverFiltro.parentElement
+            const valorDoXClicado = divPai.querySelector('p').innerHTML
+            divsBtnsFiltros.forEach((divBtnFiltro) => {
+                const divBtnFiltroValor = divBtnFiltro.querySelector('h5').innerHTML
+                if (divBtnFiltroValor == valorDoXClicado) {
+                    divBtnFiltro.classList.remove("border-2")
+                }
+            })
+            const filtro = e.target.parentElement.parentElement;
+            filtro.remove();
+        });
+    });
+}
---
+++ js/bordaBtnsFiltros.js
@@ -0,0 +1,10 @@
+const divsBtnsFiltros = document.querySelectorAll('.borda')
+btnsFiltrosBorda()
+
+function btnsFiltrosBorda() {
+    divsBtnsFiltros.forEach((divBtnFiltro) => {
+        divBtnFiltro.addEventListener('click', () => {
+            divBtnFiltro.classList.toggle('border-2')
+        })
+    })
+}
